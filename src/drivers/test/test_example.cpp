5adcbe3e8107c4838c6cbb47e50be937b69a286c
jacqueline <me@jacqueline.id.au>
2022-11-08 15:06:50 +1100

    Add readme, fix ignore test

--- src/drivers/test/test_example.cpp
+++ src/drivers/test/test_example.cpp
@@ -4,6 +4,6 @@ TEST_CASE("Example test case", "[cooltag]") {
     REQUIRE ( 1 == 1 );
 }
 
-TEST_CASE("test that doesn't run", "[cooltag][ignore]") {
+TEST_CASE("test that doesn't run", "[cooltag][!mayfail]") {
     REQUIRE ( 0 == 1 );
 }
