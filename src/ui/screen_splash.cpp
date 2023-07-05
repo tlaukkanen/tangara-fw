06ce512ce93eb17fddc735b7c65c2944e08e5b84
jacqueline <me@jacqueline.id.au>
2023-07-05 20:28:51 +1000

    Fix background color

--- src/ui/screen_splash.cpp
+++ src/ui/screen_splash.cpp
@@ -6,7 +6,12 @@
 
 #include "screen_splash.hpp"
 
+#include "core/lv_obj.h"
+#include "core/lv_obj_style.h"
 #include "lvgl.h"
+#include "misc/lv_area.h"
+#include "misc/lv_color.h"
+#include "misc/lv_style.h"
 
 LV_IMG_DECLARE(splash);
 
@@ -14,6 +19,9 @@ namespace ui {
 namespace screens {
 
 Splash::Splash() {
+  lv_obj_set_style_bg_opa(root_, LV_OPA_COVER, 0);
+  lv_obj_set_style_bg_color(root_, lv_color_black(), 0);
+
   lv_obj_t *logo = lv_img_create(root_);
   lv_img_set_src(logo, &splash);
   lv_obj_center(logo);
