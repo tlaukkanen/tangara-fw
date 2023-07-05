8c02ed47b54fbd64bddb631a12316ba3b1978154
jacqueline <me@jacqueline.id.au>
2023-07-05 19:33:53 +1000

    add gay splash screen

--- src/ui/screen_splash.cpp
+++ src/ui/screen_splash.cpp
@@ -6,33 +6,20 @@
 
 #include "screen_splash.hpp"
 
-#include "core/lv_obj_pos.h"
-#include "extra/widgets/spinner/lv_spinner.h"
-#include "misc/lv_area.h"
-#include "widgets/lv_label.h"
+#include "lvgl.h"
+
+LV_IMG_DECLARE(splash);
 
 namespace ui {
 namespace screens {
 
 Splash::Splash() {
-  container_ = lv_obj_create(root_);
-  lv_obj_set_align(container_, LV_ALIGN_CENTER);
-  lv_obj_set_size(container_, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
-
-  label_ = lv_label_create(container_);
-  lv_label_set_text_static(label_, "TANGARA");
-  lv_obj_set_align(label_, LV_ALIGN_TOP_MID);
-
-  spinner_ = lv_spinner_create(container_, 1000, 60);
-  lv_obj_set_size(spinner_, 32, 32);
-  lv_obj_align_to(spinner_, label_, LV_ALIGN_OUT_BOTTOM_MID, 0, 8);
+  lv_obj_t *logo = lv_img_create(root_);
+  lv_img_set_src(logo, &splash);
+  lv_obj_center(logo);
 }
 
-Splash::~Splash() {
-  lv_obj_del(spinner_);
-  lv_obj_del(label_);
-  lv_obj_del(container_);
-}
+Splash::~Splash() {}
 
 }  // namespace screens
 }  // namespace ui
