6f4ace1dd4b9b34f95af1ba365b68624e209d147
ailurux <ailuruxx@gmail.com>
2023-08-28 14:59:29 +1000

    Custom themes and top bar styling

--- src/ui/include/themes.hpp
+++ src/ui/include/themes.hpp
@@ -4,14 +4,24 @@
 
 namespace ui {
 namespace themes {
+
+  enum class Style {
+    kMenuItem,
+    kTopBar
+  };
 class Theme {
  public:
-  Theme();
+
   void Apply(void);
   void Callback(lv_obj_t* obj);
+  void ApplyStyle(lv_obj_t* obj, Style style);
+
+  static auto instance() -> Theme*;
 
  private:
+  Theme();
   lv_style_t button_style_;
+  lv_style_t button_style_focused_;
   lv_theme_t theme_;
 };
 }  // namespace themes
