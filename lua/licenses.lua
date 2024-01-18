d1ae1a4230b413b6a92179936d431c83247c5fd2
jacqueline <me@jacqueline.id.au>
2024-01-18 14:55:27 +1100

    on second thought, let's not violate patents. 'tis a silly system.

--- lua/licenses.lua
+++ lua/licenses.lua
@@ -26,14 +26,6 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.]])
 end
 
-local function faadgpl()
-  show_license(
-    [[FAAD2 - Freeware Advanced Audio (AAC) Decoder including SBR decoding Copyright (C) 2003-2005 M. Bakker, Nero AG, http://www.nero.com This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. Any non-GPL usage of this software or parts of this software is strictly forbidden.
-The "appropriate copyright message" mentioned in section 2c of the GPLv2 must read: "Code from FAAD2 is copyright (c) Nero AG, www.nero.com" Commercial non-GPL licensing of this software is possible. For more info contact Nero AG through Mpeg4AAClicense@nero.com.]])
-end
-
 local function bsd(copyright)
   show_license(copyright .. [[
 
@@ -120,9 +112,6 @@ return function()
   end)
   library("esp-idf-lua", "MIT", function()
     mit("Copyright (C) 2019 Ruslan V. Uss")
-  end)
-  library("FAAD2", "GPL", function()
-    faadgpl()
   end)
   library("FatFs", "BSD", function()
     bsd("Copyright (C) 2022, ChaN, all right reserved.")
