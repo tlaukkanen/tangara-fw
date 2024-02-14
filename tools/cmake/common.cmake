4509ab8d6e341f7f7d92ac6e9d63ad822fe3441b
jacqueline <me@jacqueline.id.au>
2024-02-15 09:38:02 +1100

    version bump

--- tools/cmake/common.cmake
+++ tools/cmake/common.cmake
@@ -5,7 +5,7 @@
 # For more information about build system see
 # https://docs.espressif.com/projects/esp-idf/en/latest/api-guides/build-system.html
 
-set(PROJECT_VER "0.5.1")
+set(PROJECT_VER "0.5.2")
 
 # esp-idf sets the C++ standard weird. Set cmake vars to match.
 set(CMAKE_CXX_STANDARD 23)
