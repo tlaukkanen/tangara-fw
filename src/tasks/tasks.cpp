5d390c821a7fdddec5eb24a7a3a08bf8bf4be1cc
jacqueline <me@jacqueline.id.au>
2024-08-09 13:11:34 +1000

    Claw back some internal ram
     - 'main' doesn't need 12k of internal ram
     - lvgl's draw task doesn't need that much either
     - also lower the bg worker stack sizes whilst we're here, since they've got tons over headroom

--- src/tasks/tasks.cpp
+++ src/tasks/tasks.cpp
@@ -64,7 +64,7 @@ auto AllocateStack<Type::kAudioConverter>() -> std::span<StackType_t> {
 // an eye-wateringly large amount of stack.
 template <>
 auto AllocateStack<Type::kBackgroundWorker>() -> std::span<StackType_t> {
-  std::size_t size = 64 * 1024;
+  std::size_t size = 32 * 1024;
   return {static_cast<StackType_t*>(heap_caps_malloc(size, MALLOC_CAP_SPIRAM)),
           size};
 }
